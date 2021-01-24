2f68449312523eaaa4e4342ab61296b24fc8b587
Mark <mmarkk1994@gmail.com>
2021-01-24 11:48:17 +0100

    changed webshop to frontend webshop

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static('/dist/webshop'));
+app.use(express.static('/dist/frontend_webshop'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/webshop/'}),
+  res.sendFile('index.html', {root: 'dist/frontend_webshop/'}),
 );
 
 // Start the app by listening on the default Heroku port
