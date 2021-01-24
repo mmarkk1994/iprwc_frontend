ceb30927610aca25358e8e4ee620e84867cbdb0a
Mark <mmarkk1994@gmail.com>
2021-01-24 11:26:32 +0100

    commented out app.use

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express;
 
 // Serve only the static files from the dist directory
-app.use(express.static('/dist/webshop'));
+// app.use(express.static('/dist/webshop'));
 
 app.get('/*', (req, res) =>
   res.sendfile('index.html', {root: 'dist/webshop/'}),
 );
+
+// Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
