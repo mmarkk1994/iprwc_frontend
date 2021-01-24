36b54883fc33d2a55db7ee7bfe1a33a74b585146
Mark <mmarkk1994@gmail.com>
2021-01-24 11:32:39 +0100

    added () to express

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const path = require('path');
 
-const app = express;
+const app = express();
 
 // Serve only the static files from the dist directory
-// app.use(express.static('/dist/webshop'));
+app.use(express.static('/dist/webshop'));
 
 app.get('/*', (req, res) =>
   res.sendfile('index.html', {root: 'dist/webshop/'}),
