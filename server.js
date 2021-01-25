214ac14f15f3bf6aa5328184d1d45c6ebafd4c84
Mark <mmarkk1994@gmail.com>
2021-01-25 11:43:36 +0100

    -

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static('/dist/webshop/assets'));
+app.use(express.static('/dist/webshop'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/webshop/assets/'})
