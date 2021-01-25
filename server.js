151a5bbbb07c87cfe0e62b5e80b5d1177bd51760
Mark <mmarkk1994@gmail.com>
2021-01-25 10:23:12 +0100

    changed webshop to src

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static('/dist/webshop'));
+app.use(express.static('/dist/src'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/webshop/'}),
+  res.sendFile('index.html', {root: 'dist/src/'}),
 );
 
 // Start the app by listening on the default Heroku port
