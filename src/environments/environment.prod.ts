21d532c126ad5f3bc0e06592a45eb66ceb65b021
Mark <mmarkk1994@gmail.com>
2021-02-03 15:05:43 +0100

    changed API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cors-anywhere.herokuapp.com/https://iprwc-backend.herokuapp.com/'
+  apiUrl: 'https://iprwc-backend.herokuapp.com/'
 };
 
 
